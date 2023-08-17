c217dd7a520c1bf37126af272cf7c2e14cd7ebd3
PeiYun <ollylee8520@gmail.com>
2023-08-17 15:30:30 -0600

    update new mongoDB database file

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://ollyhite:leehite@cluster0.ycxk7ur.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://ollyhite:leehite@cluster0.npzhb9j.mongodb.net/?retryWrites=true&w=majority",
+
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
